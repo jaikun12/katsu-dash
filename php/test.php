675f8d5fe7060f18ab379cea5eca28080e784fe2
boazcstrike <boazcstrike@yahoo.com.sg>
2017-03-07 18:34:07 +0800

    Minor test.php

--- php/test.php
+++ php/test.php
@@ -3,7 +3,7 @@
 	include("dbconnect.php");
 
 	$query = $connection->prepare("INSERT INTO report_users_table (username, password, is_admin, firstname, middlename, lastname, contact_num, email, is_active, created_by) VALUES (?,?,?,?,?,?,?,?,?,?);");
-	$query->bind_param("ssisssssis", $username, $init_pass, $is_admin, $firstname, $middlename, $lastname,  $is_active, $contact_num, $email, $created_by);
+	$query->bind_param("ssisssssis", $username, $init_pass, $is_admin, $firstname, $middlename, $lastname, $contact_num, $email, $is_active, $created_by);
 
 	$query2 = $connection->prepare("SELECT * FROM report_users_table WHERE username = ? AND password = ?;");
 	$query2->bind_param("ss",$username,$init_pass);
